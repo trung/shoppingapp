bd7221fba5908ee8b0022d45a64e6fecaeff0a7e
thinhqq <thinhqq@gmail.com>
2012-04-25 17:30:44 +0000

    git-svn-id: https://subversion.assembla.com/svn/tommyquang/syscan/trunk/shoppingapp@150 f6d81c03-b33d-49bf-96b9-21566404a5c6

---
+++ src/org/telokers/servlet/api/EditUserServlet.java
@@ -0,0 +1,24 @@
+
+/**
+ @author tommyquangqthinh
+*/
+
+package org.telokers.servlet.api;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditUserServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		RequestDispatcher rp = getServletContext().getRequestDispatcher("/WEB-INF/jsp/editUser.jsp");
+		rp.forward(req, resp);
+	}
+}
+
